c5aaf3f45a8e55e43d9ce84abda0cb70bafcc438
ruilima1 <ruilima1997@gmail.com>
2020-02-19 00:56:39 +0000

    more console log tests

--- Productivity-Hub/src/app.jsx
+++ Productivity-Hub/src/app.jsx
@@ -7,12 +7,14 @@ import { jsx } from '@emotion/core';
 //set the app to load help.html on load
 export default class App extends React.Component {
   render() {
+    console.log('Rui Lima is gay!!');
     return (
       <div>
         <div>
           <webview src="index.html" />
         </div>
       </div>
+      {console.log('Rui Lima is gay')}
     );
   }
 }
--- Productivity-Hub/src/appLoader.jsx
+++ Productivity-Hub/src/appLoader.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 
 const render = () => {
-  console.log("AppLoader Ran");
   const App = require('./app').default;
   ReactDOM.render(<AppContainer><App /></AppContainer>, document.getElementById('App'));
 }
